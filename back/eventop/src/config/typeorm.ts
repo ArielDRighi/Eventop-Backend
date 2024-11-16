9f5a260f7813842a5cfb890977a41bf1a70dccc2
Damian de Jesus Tymoszuk <tymoszukdamiandejesus@gmail.com>
2024-11-16 00:44:35 -0300

    deploy

--- back/eventop/src/config/typeorm.ts
+++ back/eventop/src/config/typeorm.ts
@@ -1,4 +1,5 @@
 import { registerAs } from '@nestjs/config';
+import { subscribe } from 'diagnostics_channel';
 import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
@@ -6,17 +7,18 @@ dotenvConfig({ path: '.env' });
 
 const config = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   synchronize: true,
   autoLoadEntities: true,
-  logging: true,
+  logging: false,
   dropSchema: false,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
+  subscribers: [],
 };
 
 export default registerAs('typeorm', () => config);
