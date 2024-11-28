14eaac22c245d1fa9f6037eba63e0a995a539a33
Ariel Righi <arieldavidrighi@gmail.com>
2024-11-28 09:41:08 -0300

    SCRUM-138_Ariel_Righi_Lista_Negra
    Se ha implementado una nueva funcionalidad que permite almacenar los correos electrónicos de los usuarios baneados permanentemente. Esto asegura que los usuarios no puedan registrarse nuevamente con el mismo correo electrónico después de ser baneados permanentemente.
    Se creó una nueva entidad BannedEmail para almacenar los correos electrónicos de los usuarios baneados permanentemente.
    Se actualizó el servicio de usuarios para manejar la creación y almacenamiento de registros de BannedEmail cuando un usuario es baneado permanentemente.

--- back/eventop/src/payment/payment.module.ts
+++ back/eventop/src/payment/payment.module.ts
@@ -10,11 +10,13 @@ import { Event } from '@app/events/entities/events.entity';
 import { Category } from '@app/categories/entities/categories.entity';
 import { Comment } from '@app/users/entities/comments.entity';
 import { MailModule } from '@app/mail/mail.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Location, Event, Category, Comment]),
     MailModule,
+    UsersModule,
   ],
   controllers: [PaymentController],
   providers: [PaymentService, UserService, EventService],
