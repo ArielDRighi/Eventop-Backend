3666030aea5b2a6723e7e14e8f1d09a6b44b3681
Ariel Righi <arieldavidrighi@gmail.com>
2024-12-04 13:09:23 -0300

    Update main.ts

--- back/eventop/src/main.ts
+++ back/eventop/src/main.ts
@@ -9,17 +9,17 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  const corsOptions: CorsOptions = {
-    origin: [
-      'https://eventop-frontend.vercel.app', // URL de producción
-      'https://eventop-frontend-6zhm7o77f-ariel-righis-projects.vercel.app',
-      'http://localhost:3000/',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  };
+  // const corsOptions: CorsOptions = {
+  //   origin: [
+  //     'https://eventop-frontend.vercel.app', // URL de producción
+  //     'https://eventop-frontend-6zhm7o77f-ariel-righis-projects.vercel.app',
+  //     'http://localhost:3000/',
+  //   ],
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   credentials: true,
+  // };
 
-  app.enableCors(corsOptions);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Eventop')
